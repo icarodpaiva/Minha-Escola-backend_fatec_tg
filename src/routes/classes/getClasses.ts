d14d83af692b553748ce1cb06c8f17f0a3438783
Icaro Paiva <admin@tolky.to>
2023-12-13 02:21:18 -0300

    feat: classes

--- src/routes/classes/getClasses.ts
+++ src/routes/classes/getClasses.ts
@@ -2,19 +2,21 @@ import { supabase } from "../../databases/supabase"
 
 import type { Request, Response } from "express"
 
+interface Groups {
+  groups: {
+    id: number
+    subjects: {
+      name: string
+    }
+    classes: Class[]
+  }
+}
+
 interface Class {
   id: number
   start_time: string
   end_time: string
-  groups: Groups
   locations: Locations
-}
-
-interface Groups {
-  id: number
-  subjects: {
-    name: string
-  }
 }
 
 interface Locations {
@@ -23,41 +25,98 @@ interface Locations {
   classroom: string
 }
 
+interface Teachers {
+  group_id: number
+  users: {
+    name: string
+  }
+}
+
+interface ClassWithAdditionalInfos extends Class {
+  subject: string
+  teacher: string
+}
+
 export async function getClasses(_: Request, res: Response) {
   try {
-    const {
-      data: classes,
-      error: classesError
-    }: { data: Class[] | null; error: any } = await supabase
-      .from("classes")
+    const { data: groups, error: groupsError } = (await supabase
+      .from("users_groups")
       .select(
         `
-          id,
-          start_time,
-          end_time,
           groups(
             id,
-            subjects(name)
-          ),
-          locations(
-            building,
-            floor,
-            classroom
-          )            
+            subjects(name),
+            classes (
+              id,
+              start_time,
+              end_time,
+              locations(
+                building,
+                floor,
+                classroom
+              )
+            )
+          )
         `
       )
+      .eq("user_id", 7) // Authenticated user id
+      .eq("groups.classes.weekday_id", 3) // Current weekday id
+      .not("groups", "is", null)
+      .not("groups.classes", "is", null)) as {
+      data: Groups[] | null
+      error: any
+    }
 
-    if (classesError) {
+    if (groupsError) {
       res.status(500).send("Internal server error")
       return
     }
 
-    if (!classes || classes.length === 0) {
+    if (!groups || groups.length === 0) {
       res.status(404).send("Not found")
       return
     }
 
-    res.status(200).send(classes)
+    const groupsIds = groups.map(({ groups: { id } }) => id)
+
+    const { data: teachers, error: teachersError } = (await supabase
+      .from("users_groups")
+      .select("group_id, users(name)")
+      .in("group_id", groupsIds)
+      .eq("users.access_level_id", 3) // Teacher access level id
+      .not("users", "is", null)) as {
+      data: Teachers[] | null
+      error: any
+    }
+
+    if (teachersError) {
+      res.status(500).send("Internal server error")
+      return
+    }
+
+    if (!teachers || teachers.length === 0) {
+      res.status(404).send("Not found")
+      return
+    }
+
+    const formattedClasses = groups.reduce(
+      (acc, { groups: { id, classes, subjects } }) => {
+        const teacher = teachers.find(
+          ({ group_id }) => group_id === id
+        ) as Teachers
+
+        const classesWithAdditionalInfo = classes.map(classItem => ({
+          ...classItem,
+          subject: subjects.name,
+          teacher: teacher.users.name
+        }))
+
+        return [...acc, ...classesWithAdditionalInfo]
+      },
+      [] as ClassWithAdditionalInfos[]
+    )
+
+    res.status(200).send(formattedClasses)
   } catch (error) {
     console.log(error)
     res.status(500).send("Internal server error")
